3dc76ec35624ffb7dec1b8439609ff5d2ba09fec
Gustavo Xavier <j0urn3yemail@gmail.com>
2015-06-18 19:50:08 +0100

    Initial (#2) commit

---
+++ Output/Plugins/GenDebugInfoFile.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\Gustavo\Documents\Visual Studio 2015\Projects\StasisChamberKSP\Output\Plugins"
+"C:\Program Files (x86)\Unity\Editor\Data\MonoBleedingEdge\bin\cli.bat" "C:\Program Files (x86)\Unity\Editor\Data\MonoBleedingEdge\lib\mono\4.0\pdb2mdb.exe" StasisChamberKSP.dll
---
+++ StasisChamberKSP/StasisChamber.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+
+namespace StasisChamberKSP
+{
+    public class StasisChamber : PartModule
+    {
+        public List<ProtoCrewMember> Crew = FlightGlobals.ActiveVessel.GetVesselCrew();
+        [KSPEvent(guiActive = true, guiName = "FreezeCrew", active = true, name = "FreezeCrew")]
+        public void FreezeCrew()
+        {
+            this.vessel.DespawnCrew();
+            Events["FreezeCrew"].active = false;
+            Events["UnFreezeCrew"].active = true;
+            #if DEBUG
+                Debug.Log("Crew Frozen/DeSpawned.")
+            #endif
+        }
+
+        [KSPEvent(guiActive = true, guiName = "UnFreezeCrew", active = false, name = "UnFreezeCrew")]
+        public void UnFreezeCrew()
+        {
+            vessel.SpawnCrew();
+            Events["UnFreezeCrew"].active = false;
+            Events["FreezeCrew"].active = true;
+            #if DEBUG
+                Debug.Log("Crew UnFrozen/Spawned.")
+            #endif
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+#if false
+        //This will kick us into the save called default and set the first vessel active
+        [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+        public class Debug_AutoLoadPersistentSaveOnStartup : MonoBehaviour
+        {
+            //use this variable for first run to avoid the issue with when this is true and multiple addons use it
+            public static bool first = true;
+            public void Start()
+            {
+                //only do it on the first entry to the menu
+                if (first)
+                {
+                    first = false;
+                    HighLogic.SaveFolder = "default";
+                    Game game = GamePersistence.LoadGame("persistent", HighLogic.SaveFolder, true, false);
+
+                    if (game != null && game.flightState != null && game.compatible)
+                    {
+                        Int32 FirstVessel;
+                        Boolean blnFoundVessel = false;
+                        for (FirstVessel = 0; FirstVessel < game.flightState.protoVessels.Count; FirstVessel++)
+                        {
+                            //This logic finds the first non-asteroid vessel
+                            if (game.flightState.protoVessels[FirstVessel].vesselType != VesselType.SpaceObject &&
+                                game.flightState.protoVessels[FirstVessel].vesselType != VesselType.Unknown)
+                            {
+                                ////////////////////////////////////////////////////
+                                ////////PUT ANY OTHER LOGIC YOU WANT IN HERE////////
+                                ////////////////////////////////////////////////////
+                                blnFoundVessel = true;
+                                break;
+                            }
+                        }
+                        if (!blnFoundVessel)
+                        {
+                            FirstVessel = 0;
+                            FlightDriver.StartAndFocusVessel(game, FirstVessel);
+                        }
+                    }
+
+                    //CheatOptions.InfiniteFuel = true;
+                }
+            }
+        }
+#endif
+
+}
